3bcd0e0db73561dd5d86e72d98c5b4625bd9518c
prhodesy <peter.rhodes@gmail.com>
2021-07-30 17:33:32 +0700

    updated app component, made footer sticky, updated home html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-starter';
+  faGithub = faGithub;
 }
