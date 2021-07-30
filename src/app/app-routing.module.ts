fa196711644ffe3efef3500235b552a1e82981d0
prhodesy <peter.rhodes@gmail.com>
2021-07-30 16:18:33 +0700

    configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from 'src/app/components/home/home.component';
+import { NotFoundComponent } from 'src/app/components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirect empty url to 'home'
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: NotFoundComponent } // Wildcard route for a 404 not found, must be last element in the list
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
