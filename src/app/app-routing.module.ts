83309073ef1ede7e18ffc0fb99868eb187e596da
prhodesy <peter.rhodes@gmail.com>
2021-08-30 21:23:11 +0700

    added components to nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CommentComponent } from 'src/app/components/comment/comment.component';
 import { HomeComponent } from 'src/app/components/home/home.component';
 import { NotFoundComponent } from 'src/app/components/not-found/not-found.component';
+import { PostComponent } from 'src/app/components/post/post.component';
+import { UserComponent } from 'src/app/components/user/user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirect empty url to 'home'
+  { path: 'comment', component: CommentComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'post', component: PostComponent },
+  { path: 'user', component: UserComponent },
   { path: '**', component: NotFoundComponent } // Wildcard route for a 404 not found, must be last element in the list
 ];
 
