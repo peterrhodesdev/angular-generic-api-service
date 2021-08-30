256b4843b34b617966a18015720b92780f0c3206
prhodesy <peter.rhodes@gmail.com>
2021-08-30 23:23:11 +0700

    changed post model to extend api base endpoint model

--- src/app/models/post.model.ts
+++ src/app/models/post.model.ts
@@ -1,5 +1,6 @@
-export interface PostModel {
-  id: number;
+import { BaseApiEndpointModel } from 'src/app/models/base-api-endpoint.model';
+
+export interface PostModel extends BaseApiEndpointModel {
   userId: number;
   title: string;
   body: string;
