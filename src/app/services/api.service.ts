cad1d65a3b3015d6ed60ba2b787e11f1a10ca594
prhodesy <peter.rhodes@gmail.com>
2021-08-31 18:01:41 +0700

    extracted obj to json string logic

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -39,35 +39,28 @@ export class ApiService {
   /* PATCH (partial update) */
 
   public patch<T>(url: string, partialT: Partial<T>): Observable<Partial<T>> {
-    let obs: Observable<Partial<T>> = this.httpClient.patch<Partial<T>>(url, JSON.stringify(partialT), this.httpOptions);
+    let jsonString: string = this.convertObjToJsonString(partialT);
+    let obs: Observable<Partial<T>> = this.httpClient.patch<Partial<T>>(url, jsonString, this.httpOptions);
     return this.applyPipeOperations(obs);
   }
 
   /* POST (create) */
 
   public post<T>(url: string, t: T): Observable<T> {
-    let obs: Observable<T> = this.httpClient.post<T>(url, JSON.stringify(t), this.httpOptions);
+    let jsonString: string = this.convertObjToJsonString(t);
+    let obs: Observable<T> = this.httpClient.post<T>(url, jsonString, this.httpOptions);
     return this.applyPipeOperations(obs);
   }
 
   /* PUT (full update) */
 
   public put<T>(url: string, t: T): Observable<T> {
-    let obs: Observable<T> = this.httpClient.put<T>(url, JSON.stringify(t), this.httpOptions);
+    let jsonString: string = this.convertObjToJsonString(t);
+    let obs: Observable<T> = this.httpClient.put<T>(url, jsonString, this.httpOptions);
     return this.applyPipeOperations(obs);
   }
 
   /* private methods */
-
-  private errorHandler(error: HttpErrorResponse): Observable<any> {
-    let errorMessage = '';
-    if(error.error instanceof ErrorEvent) { // client-side error
-      errorMessage = error.error.message;
-    } else { // server-side error
-      errorMessage = `Error status: ${error.status}, error message: ${error.message}`;
-    }
-    return throwError(errorMessage);
-  }
 
   private applyPipeOperations(obs: Observable<any>): Observable<any> {
     if (!environment.production) {
@@ -80,5 +73,19 @@ export class ApiService {
     return obs.pipe(
       catchError(this.errorHandler)
     );
+  }
+
+  private convertObjToJsonString(obj: any): string {
+    return JSON.stringify(obj);
+  }
+
+  private errorHandler(error: HttpErrorResponse): Observable<any> {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) { // client-side error
+      errorMessage = error.error.message;
+    } else { // server-side error
+      errorMessage = `Error status: ${error.status}, error message: ${error.message}`;
+    }
+    return throwError(errorMessage);
   }
 }
