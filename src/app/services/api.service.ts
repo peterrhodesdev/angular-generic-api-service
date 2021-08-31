c5192ed93b50c1179aa0c5063769f3c9df552ad0
prhodesy <peter.rhodes@gmail.com>
2021-08-31 17:45:37 +0700

    added the rest api methods

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -15,7 +15,37 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
+  /* DELETE */
+
+  public delete(url: string): Observable<any> {
+    return this.httpClient.delete(url, this.httpOptions);
+  }
+
+  /* GET (read) */
+
   public getMany<T>(url: string): Observable<T[]> {
     return this.httpClient.get<T[]>(url, this.httpOptions);
+  }
+
+  public getOne<T>(url: string): Observable<T> {
+    return this.httpClient.get<T>(url, this.httpOptions);
+  }
+
+  /* PATCH (partial update) */
+
+  public patch<T>(url: string, partialT: Partial<T>): Observable<Partial<T>> {
+    return this.httpClient.patch<Partial<T>>(url, JSON.stringify(partialT), this.httpOptions);
+  }
+
+  /* POST (create) */
+
+  public post<T>(url: string, t: T): Observable<T> {
+    return this.httpClient.post<T>(url, JSON.stringify(t), this.httpOptions);
+  }
+
+  /* PUT (full update) */
+
+  public put<T>(url: string, t: T): Observable<T> {
+    return this.httpClient.put<T>(url, JSON.stringify(t), this.httpOptions);
   }
 }
