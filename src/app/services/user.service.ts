d486b905b8a786d55d6d9305f0cbb917960e809b
prhodesy <peter.rhodes@gmail.com>
2021-09-01 22:08:55 +0700

    created user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { BaseApiEndpointService } from 'src/app/services/base-api-endpoint.service';
+import { UserModel } from 'src/app/models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService extends BaseApiEndpointService<UserModel> {
+
+  /* abstract methods from parent */
+
+  public getBaseUrl(): string {
+    return environment.API_BASE_URL_JSON_PLACEHOLDER;
+  }
+
+  public getEndpoint(): string {
+    return environment.API_ENDPOINT_JSON_PLACEHOLDER_USERS;
+  }
+
+  public getInstance(): TodoModel {
+    return new UserModel();
+  }
+}
