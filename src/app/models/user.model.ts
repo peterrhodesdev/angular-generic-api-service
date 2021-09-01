6b51c41c3d03c33cb9efc6014b3906a1c9fd7031
prhodesy <peter.rhodes@gmail.com>
2021-09-01 21:59:25 +0700

    updated model to match api

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,10 +1,30 @@
-export interface UserModel {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  //address: nested model (geo: nested model)
-  phone: string;
-  website: string;
-  //company: nested model
+import { BaseApiEndpointModel } from 'src/app/models/base-api-endpoint.model';
+
+export class UserModel extends BaseApiEndpointModel<number> {
+  name: string = '';
+  username: string = '';
+  email: string = '';
+  phone: string = '';
+  website: string = '';
+  address: AddressModel = new AddressModel();
+  company: CompanyModel = new CompanyModel();
+}
+
+export class AddressModel {
+  street: string = '';
+  suite: string = '';
+  city: string = '';
+  zipcode: string = '';
+  geo: GeoModel = new GeoModel();
+}
+
+export class GeoModel {
+  lat: string = '';
+  lng: string = '';
+}
+
+export class CompanyModel {
+  name: string = '';
+  catchPhrase: string = '';
+  bs: string = '';
 }
