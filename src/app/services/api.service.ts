636f0cf98ed3af4e1fc5ca55eb3e3ca291b60c33
prhodesy <peter.rhodes@gmail.com>
2021-09-01 12:22:27 +0700

    created and commented missing http methods

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, pipe, throwError } from 'rxjs';
 import { catchError, delay, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { ValidationHelper } from 'src/app/helpers/validation.helper';
 
 @Injectable({
   providedIn: 'root'
@@ -17,43 +18,59 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
-  /* DELETE */
+  /* public methods */
 
+  /**
+   * Performs a HTTP DELETE method request to delete a resource.
+   */
   public delete(url: string): Observable<any> {
     let obs: Observable<any> = this.httpClient.delete(url, this.httpOptions);
     return this.applyPipeOperations(obs);
   }
 
-  /* GET (read) */
-
+  /**
+   * Performs a HTTP GET method request to retrieve multiple resources.
+   */
   public getMany<T>(url: string): Observable<T[]> {
     let obs: Observable<T[]> = this.httpClient.get<T[]>(url, this.httpOptions);
     return this.applyPipeOperations(obs);
   }
 
+  /**
+   * Performs a HTTP GET method request to retrieve a single resource.
+   */
   public getOne<T>(url: string): Observable<T> {
     let obs: Observable<T> = this.httpClient.get<T>(url, this.httpOptions);
     return this.applyPipeOperations(obs);
   }
 
-  /* PATCH (partial update) */
-
+  /**
+   * Performs a HTTP PATCH method request to modfiy a resource.
+   *
+   * @throws Error From this.convertObjToJsonString
+   */
   public patch<T>(url: string, partialT: Partial<T>): Observable<Partial<T>> {
     let jsonString: string = this.convertObjToJsonString(partialT);
     let obs: Observable<Partial<T>> = this.httpClient.patch<Partial<T>>(url, jsonString, this.httpOptions);
     return this.applyPipeOperations(obs);
   }
 
-  /* POST (create) */
-
+  /**
+   * Performs a HTTP POST method request to create a resource.
+   *
+   * @throws Error From this.convertObjToJsonString
+   */
   public post<T>(url: string, t: T): Observable<T> {
     let jsonString: string = this.convertObjToJsonString(t);
     let obs: Observable<T> = this.httpClient.post<T>(url, jsonString, this.httpOptions);
     return this.applyPipeOperations(obs);
   }
 
-  /* PUT (full update) */
-
+  /**
+   * Performs a HTTP PUT method request to replace a resource.
+   *
+   * @throws Error From this.convertObjToJsonString
+   */
   public put<T>(url: string, t: T): Observable<T> {
     let jsonString: string = this.convertObjToJsonString(t);
     let obs: Observable<T> = this.httpClient.put<T>(url, jsonString, this.httpOptions);
@@ -75,7 +92,13 @@ export class ApiService {
     );
   }
 
+  /**
+   * @throws Error If the obj argument is null or undefined.
+   */
   private convertObjToJsonString(obj: any): string {
+    if (ValidationHelper.isNullOrUndefined(obj)) {
+      throw Error("Illegal Argument Error: can't convert null or undefined objects to JSON string");
+    }
     return JSON.stringify(obj);
   }
 
--- src/app/services/base-api-endpoint.service.ts
+++ src/app/services/base-api-endpoint.service.ts
@@ -6,11 +6,18 @@ import { BaseApiEndpointModel } from 'src/app/models/base-api-endpoint.model';
 import { plainToClassFromExist } from 'class-transformer';
 import { QueryParameter } from 'src/app/common/query-parameter';
 import { UrlHelper } from 'src/app/helpers/url.helper';
+import { ValidationHelper } from 'src/app/helpers/validation.helper';
 
 @Injectable({
   providedIn: 'root'
 })
 export abstract class BaseApiEndpointService<ID extends number | string, T extends BaseApiEndpointModel<ID>> {
+
+  private pipeOperations = pipe(
+    map(response => {
+      return plainToClassFromExist(this.getInstance(), response);
+    })
+  );
 
   constructor(private apiService: ApiService) { }
 
@@ -20,8 +27,28 @@ export abstract class BaseApiEndpointService<ID extends number | string, T exten
   public abstract getEndpoint(): string;
   public abstract getInstance(): T;
 
-  /* GET (read) */
+  /* public methods */
 
+  /**
+   * Create a new model.
+   *
+   * @throws Error From this.apiService.post
+   */
+  public create(t: T): Observable<T> {
+    return this.apiService.post<T>(this.endpointUrl(), t)
+      .pipe(this.pipeOperations);
+  }
+
+  /**
+   * Delete a model by id.
+   */
+  public delete(id: ID): Observable<any> {
+    return this.apiService.delete(this.endpointUrlWithId(id));
+  }
+
+  /**
+   * Get multiple models.
+   */
   public getMany(params: QueryParameter[] = []): Observable<T[]> {
     let queryString: string = UrlHelper.getValidQueryString(params);
     let requestUrl: string = this.endpointUrl() + queryString;
@@ -33,19 +60,51 @@ export abstract class BaseApiEndpointService<ID extends number | string, T exten
         }));
   }
 
+  /**
+   * Get a single model by id.
+   */
   public getOne(id: ID): Observable<T> {
-    let requestUrl: string = `${this.endpointUrl()}${id}`;
+    return this.apiService.getOne<T>(this.endpointUrlWithId(id))
+      .pipe(this.pipeOperations);
+  }
 
-    return this.apiService.getOne<T>(requestUrl)
-      .pipe(
-        map(response => { 
-          return plainToClassFromExist(this.getInstance(), response);
-        }));
+  /**
+   * Perform a full update of a model specified by id.
+   *
+   * @throws Error If the id field of the model is defined and doesn't match the id argument.
+   * @throws Error From this.apiService.put
+   */
+  public updateFull(id: ID, t: T): Observable<T> {
+    if (!ValidationHelper.isNullOrUndefined(t.id) && t.id! !== id) {
+      throw Error(`Illegal Argument Error: model id ${t.id} doesn't match argument id ${id}`);
+    }
+
+    return this.apiService.put<T>(this.endpointUrlWithId(id), t)
+      .pipe(this.pipeOperations);
+  }
+
+  /**
+   * Perform a partial update of a model specified by id.
+   *
+   * @throws Error TODO
+   * @throws Error From this.apiService.put
+   */
+  public updatePartial(id: ID, partialT: Partial<T>): Observable<Partial<T>> {
+    if (ValidationHelper.isNullOrUndefined(partialT)) {
+      throw Error("Illegal Argument Error: object must be defined");
+    }
+
+    return this.apiService.patch<T>(this.endpointUrlWithId(id), partialT)
+      .pipe(this.pipeOperations);
   }
 
   /* private methods */
 
   private endpointUrl(): string {
     return `${this.getBaseUrl()}${this.getEndpoint()}`;
+  }
+
+  private endpointUrlWithId(id: ID): string {
+    return `${this.endpointUrl()}${id}`;
   }
 }
