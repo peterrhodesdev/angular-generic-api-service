dbc7bf62e8c35062d0cabed9deeff53d5b9dab44
prhodesy <peter.rhodes@gmail.com>
2021-09-01 21:40:18 +0700

    extracted common environment variables

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,5 @@
+export const commonEnv = {
+  API_BASE_URL_JSON_PLACEHOLDER: 'https://jsonplaceholder.typicode.com/',
+  API_ENDPOINT_JSON_PLACEHOLDER_TODOS: 'todos/',
+  API_ENDPOINT_JSON_PLACEHOLDER_USERS: 'users/',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
-export const environment = {
+import { commonEnv } from "./environment.common";
+
+const env = {
   production: false,
-  API_BASE_URL_JSON_PLACEHOLDER: 'https://jsonplaceholder.typicode.com/',
-  API_ENDPOINT_JSON_PLACEHOLDER_TODOS: 'todos/',
 };
+
+export const environment = { ...commonEnv, ...env };
