aecfb6c7349da8b50e5c403c9edd8cace728d918
prhodesy <peter.rhodes@gmail.com>
2021-09-02 00:06:18 +0700

    called nested object method

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -26,6 +26,34 @@ export class AddressModel {
 export class GeoModel {
   lat: string = '';
   lng: string = '';
+
+  public toDegreesMinutesSeconds(): string {
+    let latDecimal: number = parseFloat(this.lat);
+    let lngDecimal: number = parseFloat(this.lng);
+
+    let [latDegrees, latMinutes, latSeconds] = this.convertDecimalToDegreesMinutesSeconds(latDecimal);
+    let [lngDegrees, lngMinutes, lngSeconds] = this.convertDecimalToDegreesMinutesSeconds(lngDecimal);
+
+    let latCardinal = latDecimal >= 0 ? "N" : "S";
+    var lngCardinal = lngDecimal >= 0 ? "E" : "W";
+
+    return this.degreesMinutesSecondsToString(latDegrees, latMinutes, latSeconds) + ` ${latCardinal}, `
+      + this.degreesMinutesSecondsToString(lngDegrees, lngMinutes, lngSeconds) + ` ${lngCardinal}`;
+  }
+
+  private degreesMinutesSecondsToString(degrees: number, minutes: number, seconds: number): string {
+    return `${degrees}\u00B0 ${minutes}' ${seconds}"`;
+  }
+
+  private convertDecimalToDegreesMinutesSeconds(coordinate: number): [number, number, number] {
+    let abs: number = Math.abs(coordinate);
+    let degrees: number = Math.floor(abs);
+    let minutesDecimal: number = (abs - degrees) * 60;
+    let minutes: number = Math.floor(minutesDecimal);
+    let seconds: number = Math.floor((minutesDecimal - minutes) * 60);
+    
+    return [degrees, minutes, seconds];
+  }
 }
 
 export class CompanyModel {
