0b38ea078a4b7d163f0612fba16d246d98243104
prhodesy <peter.rhodes@gmail.com>
2021-09-04 00:02:30 +0700

    installed http-status-codes, added routing for http status codes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from 'src/app/components/home/home.component';
+import { HttpStatusCodesComponent } from 'src/app/components/http-status-codes/http-status-codes.component';
 import { NotFoundComponent } from 'src/app/components/not-found/not-found.component';
 import { TodosComponent } from 'src/app/components/todos/todos.component';
 import { UsersComponent } from 'src/app/components/users/users.component';
@@ -9,6 +10,7 @@ import { UsersComponent } from 'src/app/components/users/users.component';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirect empty url to 'home'
   { path: 'home', component: HomeComponent },
+  { path: 'http-status-codes', component: HttpStatusCodesComponent },
   { path: 'todos', component: TodosComponent },
   { path: 'users', component: UsersComponent },
   { path: '**', component: NotFoundComponent } // Wildcard route for a 404 not found, must be last element in the list
--- src/app/components/http-status-codes/http-status-codes.component.ts
+++ src/app/components/http-status-codes/http-status-codes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpStatusCode } from '@angular/common/http';
+import { ReasonPhrases, StatusCodes, getReasonPhrase, getStatusCode } from 'http-status-codes';
 
 @Component({
   selector: 'app-http-status-codes',
@@ -8,13 +8,26 @@ import { HttpStatusCode } from '@angular/common/http';
 })
 export class HttpStatusCodesComponent implements OnInit {
 
-  public httpStatusCodes: HttpStatusCode[];
+  public httpStatusCodes: number[];
+  public selectedHttpStatusCode: number;
 
   constructor() {
-    this.httpStatusCodes = [];//Object.values(HttpStatusCode);
+    this.httpStatusCodes = Object.values(StatusCodes)
+      .filter(value => typeof value === 'number')
+      .map(value => value as number)
+      .sort((n1,n2) => n1 - n2);
+    this.selectedHttpStatusCode = this.httpStatusCodes[0];
+    console.log(this.httpStatusCodes)
   }
 
   ngOnInit(): void {
   }
 
+  public getStatusCodeDescription(statusCode: number): string {
+    return statusCode + ': ' + getReasonPhrase(statusCode);
+  }
+
+  public changeSelectedHttpStatusCode(statusCode: number) {
+    this.selectedHttpStatusCode = statusCode;
+  }
 }
