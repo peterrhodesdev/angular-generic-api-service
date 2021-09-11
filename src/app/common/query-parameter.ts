d5ae20904760a47ad4449160337267423ad9bfb4
prhodesy <peter.rhodes@gmail.com>
2021-09-11 13:24:33 +0700

    undefined filtered out by qs

--- src/app/common/query-parameter.ts
+++ src/app/common/query-parameter.ts
@@ -1,8 +1,8 @@
 export class QueryParameter {
   name: string;
-  value?: string;
+  value: string;
 
-  constructor(name: string, value?: string) {
+  constructor(name: string, value: string) {
     this.name = name;
     this.value = value;
   }
